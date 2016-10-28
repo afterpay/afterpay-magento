f0a1ebbb1d43a56796d016d36989205c41ee4dcb
Andrew Rollason <arollason@fontis.com.au>
2016-10-28 12:31:35 +1100

    Fix upgrade scripts and fix bug with order cancellation
    The upgrade scripts that were included for different version to 0.13.0
    would all be run if someone upgraded from that version. Only one upgrade
    script is required to the 0.13.0 version so the rest were removed.
    
    The 0.13.0 install script was incorrect, and didn't include most
    changes. If someone fresh installed version 0.13.0 the extension
    wouldn't work.
    
    When an order is cancelled the cancel action would find the last order
    placed by that customer and cancel it. With the new API method no order
    is created when the cancel action is run, so it would find any orders
    placed previously. If a customer placed and order that was in the
    pending state (eg: a direct deposit method) and then cancelled an
    afterpay payment, their original order would be cancelled. This change
    adds a check for the API version before continuing with the order
    cancellation code.

--- src/app/code/community/Afterpay/Afterpay/sql/afterpay_setup/install-0.13.0.php
+++ src/app/code/community/Afterpay/Afterpay/sql/afterpay_setup/install-0.13.0.php
@@ -15,6 +15,18 @@ $installer->startSetup();
  */
 
 try{
+    // add columns to sales/order_payment
+    $table = $installer->getTable('sales_flat_order_payment');
+    $installer->getConnection()->addColumn($table, "afterpay_token", "varchar(255) DEFAULT NULL COMMENT 'Afterpay Order Token'");
+    $installer->getConnection()->addColumn($table, "afterpay_order_id", "varchar(255) DEFAULT NULL COMMENT 'Afterpay Order ID'");
+    $installer->getConnection()->addColumn($table, "afterpay_fetched_at", "TIMESTAMP NULL");
+
+    // add new status and map it to Payment Review state
+    $status = 'afterpay_payment_review';
+    $state  = Mage_Sales_Model_Order::STATE_PAYMENT_REVIEW;
+    $installer->run("INSERT INTO `{$this->getTable('sales_order_status')}` (`status`, `label`) VALUES ('{$status}', 'Afterpay Processing');");
+    $installer->run("INSERT INTO `{$this->getTable('sales_order_status_state')}` (`status`, `state`, `is_default`) VALUES ('{$status}', '{$state}', '0');");
+
 	$table = $installer->getTable('sales/quote_payment');
 	$installer->getConnection()->addColumn($table, 'afterpay_token', "varchar(255) DEFAULT NULL COMMENT 'Afterpay Order Token'");
 	$installer->getConnection()->addColumn($table, 'afterpay_order_id', "varchar(255) DEFAULT NULL COMMENT 'Afterpay Order ID'");	
